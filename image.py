d68c41551497625bd762dfe11db059613021d6cb
taha <mtahakhan93@gmail.com>
2021-08-26 11:35:41 +0500

    Changes in code

--- image.py
+++ image.py
@@ -9,10 +9,10 @@ def wrap_image(image, N):
     
     for i in range(0, image.shape[0]):
         for j in range(0, image.shape[1]):
-            for k in range(0,3):
-                q, mod = divmod(image[i][j][k], np.power(2, N))
-                wrapped_image[i][j][k] = mod
-                roll_over[i][j][k] = q 
+            #for k in range(0,3):
+                q, mod = divmod(image[i][j], np.power(2, N))
+                wrapped_image[i][j] = mod
+                roll_over[i][j] = q 
     
     return wrapped_image, roll_over
             
@@ -47,8 +47,9 @@ ax1, ax2, ax3 = ax.ravel()
 fig.colorbar(ax1.imshow(image), ax=ax1)
 ax1.set_title('Original')
 
-fig.colorbar(ax2.imshow(image_wrapped), ax=ax2)
+
+fig.colorbar(ax2.imshow(image_wrapped.astype(np.uint8)), ax=ax2)
 ax2.set_title('Wrapped phase')
 
-fig.colorbar(ax3.imshow(image_unwrapped, cmap='gray'), ax=ax3)
+fig.colorbar(ax3.imshow(image_unwrapped.astype(np.uint8)), ax=ax3)
 ax3.set_title('UnWrapped phase')
